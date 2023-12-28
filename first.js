f283be8f9df4cc0f2edb475905ca913bb8437ebe
aditya <aditya.as207@gmail.com>
2023-12-28 20:47:59 +0530

    data types

--- first.js
+++ first.js
@@ -1,1 +1,18 @@
 // starting to learn js
+
+const accId = 1
+let email = "aditya.as207@gmail.com"
+var acc_pass = "23"
+
+
+console.table([accId,email,acc_pass]) //used to print mutliple values in table format
+
+/*
+
+prefer not to use var because of issue in block scope and function scope
+use let instead of var
+
+*/
+
+
+
---
+++ second.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code in newer version of java
+
+// alert("hello") we are using nodejs and not browser
+
+
+
+// data types 
+
+
+// number 
+// bigint 
+// string 
+// boolean 
+// null => standalone value
+// undefined 
+// symbol => unique 
+
+//object 
+
+console.log(typeof("")); // returns the type of the argument
+
+
+console.log(typeof(null)); // gives object
+console.log(typeof(undefined)); // gives undefined 
+
+
